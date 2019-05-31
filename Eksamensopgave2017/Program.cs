aee96b72e52a858611bea6d2c4b800e57d4e69d4
Christoffer <chollensen1999@gmail.com>
2019-05-31 15:18:11 +0200

    First commit

---
+++ Eksamensopgave2017/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Eksamensopgave2017
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
